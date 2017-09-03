6e073a72218092b20de222c1eb4dd571c543b5d4
Hiroaki Yutani <yutani.ini@gmail.com>
2017-09-03 23:02:55 +0900

    improve test description

--- tests/testthat/test-extract_codelist_urls.R
+++ tests/testthat/test-extract_codelist_urls.R
@@ -1,6 +1,6 @@
 context("extract_codelist_urls")
 
-test_that("extract_codelist_urls() randomly works", {
+test_that("extract_codelist_urls() works", {
   d <- extract_codelist_urls("test-split.html")
   expect_s3_class(d, "data.frame")
   expect_equal(nrow(d), 1L)
