4c161b9861a8c8e100c8dbe885335983645126a3
Hiroaki Yutani <yutani.ini@gmail.com>
2017-09-04 23:58:05 +0900

    merge a header without rows to the next table

--- R/split_table.R
+++ R/split_table.R
@@ -6,16 +6,18 @@ split_table <- function(tr_nodeset, pattern_lefttop_cell = ZOKUSEI_PATTERN) {
   tr_nodeset <- tr_nodeset %>%
     purrr::keep(~ length(rvest::html_nodes(., "td")) > 0)
 
-  has_bgcolor_tr <- !is.na(purrr::map_chr(tr_nodeset, rvest::html_attr, "bgcolor"))
+  is_tr_headerish <- !is.na(purrr::map_chr(tr_nodeset, rvest::html_attr, "bgcolor"))
 
-  has_bgcolor_tds <- tr_nodeset %>%
+  are_tds_headerish <- tr_nodeset %>%
     purrr::map(rvest::html_nodes, "td") %>%
     purrr::map(purrr::map_chr, rvest::html_attr, "bgcolor") %>%
     purrr::map(~ !is.na(.))
 
-  has_bgcolor_first_td <- purrr::map_lgl(has_bgcolor_tds, dplyr::first)
-  is_start_of_different_table <- has_bgcolor_tr | has_bgcolor_first_td
-  indices_of_table_top <- which(is_start_of_different_table)
+  is_first_td_headerish <- purrr::map_lgl(are_tds_headerish, 1L)
+  are_all_tds_headerish  <- purrr::map_lgl(are_tds_headerish, all)
+
+  is_start_of_different_table <- is_tr_headerish | is_first_td_headerish
+  indices_table_top <- which(is_start_of_different_table)
 
   # filter out rows without bgcolor ----------------
   td_nodeset_lefttop_of_table <- rvest::html_node(tr_nodeset[is_start_of_different_table], "td")
@@ -30,25 +32,34 @@ split_table <- function(tr_nodeset, pattern_lefttop_cell = ZOKUSEI_PATTERN) {
   # expected_rows can overwrap
   preserve_row <- rep(FALSE, length(tr_nodeset))
   for (i in seq_along(expected_rows)) {
-    preserve_row[indices_of_table_top[i]:(indices_of_table_top[i] + expected_rows[i] - 1)] <- TRUE
+    preserve_row[indices_table_top[i]:(indices_table_top[i] + expected_rows[i] - 1)] <- TRUE
   }
 
   tr_nodeset <- tr_nodeset[preserve_row]
   is_start_of_different_table <- is_start_of_different_table[preserve_row]
-  has_bgcolor_tr <- has_bgcolor_tr[preserve_row]
-  has_bgcolor_tds <- has_bgcolor_tds[preserve_row]
+  is_tr_headerish <- is_tr_headerish[preserve_row]
+  are_all_tds_headerish <- are_all_tds_headerish[preserve_row]
+  expected_rows <- expected_rows[preserve_row]
 
   # split table ----------------------
+  is_header <- is_tr_headerish | are_all_tds_headerish
+  is_header_without_rows <- is_header & expected_rows == 1L
+  # if it is next to the table without rows, it is probably the part of the table
+  is_start_of_different_table <- is_start_of_different_table & !c(FALSE, dplyr::lag(is_header_without_rows)[-1])
   table_id <- cumsum(is_start_of_different_table)
   tr_nodeset_list <- split(tr_nodeset, table_id)
 
   # filter out tables if it doesn't have a header or the filter_pattern doesn't match -----------------
-  has_bgcolor_all_td <- purrr::map_lgl(has_bgcolor_tds, all)
-  is_header <- has_bgcolor_tr | has_bgcolor_all_td
   has_header <- is_header[is_start_of_different_table]
 
   unname(tr_nodeset_list[has_header])
 }
+
+
+get_tr_metadata <- function(tr_nodeset) {
+
+}
+
 
 #' @export
 split_tables <- function(tr_nodeset_list, pattern_lefttop_cell = ZOKUSEI_PATTERN) {
--- tests/testthat/test-split_table.R
+++ tests/testthat/test-split_table.R
@@ -23,3 +23,11 @@ test_that("split_table works for HTML with two tables and one ignorable table", 
   expect_equal(length(tr_nodeset_list), 2L)
   expect_equivalent(purrr::map_int(tr_nodeset_list, length), c(3L, 4L))
 })
+
+test_that("split_table works for HTML with tables without rows", {
+  tr_nodeset <- rvest::html_nodes(xml2::read_html("test-split4.html"), "tr")
+  tr_nodeset_list <- split_table(tr_nodeset)
+
+  expect_equal(length(tr_nodeset_list), 2L)
+  expect_equivalent(purrr::map_int(tr_nodeset_list, length), c(3L, 2L))
+})
