c0d8e155a5e06c5eadd4a429eda7b3f3397c7f20
Hiroaki Yutani <yutani.ini@gmail.com>
2017-09-05 19:36:02 +0900

    export a function to get tr nodeset

--- R/read_table.R
+++ R/read_table.R
@@ -2,11 +2,15 @@
 #'
 #' @export
 read_kokudosuuchi_table <- function(html_file) {
-  tables <- xml2::read_html(html_file, encoding = "CP932") %>%
-    rvest::html_nodes(css = "table table")
-
-  tr_nodeset_list <- purrr::map(tables, rvest::html_nodes, css = "tr")
+  tr_nodeset_list <- get_tr_nodeset_from_html(html_file)
 
   tr_nodeset_list_split <- split_tables(tr_nodeset_list)
   parse_tables(tr_nodeset_list_split)
+}
+
+#' @export
+get_tr_nodeset_from_html <- function(html_file) {
+  xml2::read_html(html_file, encoding = "CP932") %>%
+    rvest::html_nodes(css = "table table") %>%
+    purrr::map(rvest::html_nodes, css = "tr")
 }
