ba3e3d542468567ac3f2444bcd72ad0f5edc761c
Hiroaki Yutani <yutani.ini@gmail.com>
2017-09-05 19:36:20 +0900

    improve PATTERN (but this is not enough)

--- R/kokudosuuchiUtils-package.R
+++ R/kokudosuuchiUtils-package.R
@@ -8,6 +8,7 @@ NULL
 
 HTML_DIR <- "downloaded_html"
 
-ZOKUSEI_PATTERN <- "\u5c5e\u6027\u60c5\u5831|\u5730\u7269\u60c5\u5831"
+# possibly empty e.g.: http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-L02-v2_3.html
+ZOKUSEI_PATTERN <- "\u5c5e\u6027\u60c5\u5831|\u5730\u7269\u60c5\u5831|^\\s*$"
 
 COLNAME_PATTERN <- "(?<=^\u5c5e\u6027(\u60c5\u5831|\u540d|\u9805\u76ee))(\\s*[\uff08\\(].*[\uff09\\)])$"
