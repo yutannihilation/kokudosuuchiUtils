b8790e523cc345de0f91de859d74a790532fafc7
Hiroaki Yutani <yutani.ini@gmail.com>
2017-09-05 19:36:30 +0900

    add failing test

--- tests/testthat/test-parse_table.R
+++ tests/testthat/test-parse_table.R
@@ -7,9 +7,8 @@ parse_test_table <- function(html_file) {
 test_that("parse_table() works for for HTML with two tables + note", {
   l <- parse_test_table("with-note.html")
 
-  expect_equal(nrow(l[[1]]), 2L)
+  expect_equivalent(purrr::map_int(l, nrow), c(2L, 3L))
   expect_equal(colnames(l[[1]]), c("\u5c5e\u6027\u540d", "\u8aac\u660e", "\u5c5e\u6027\u306e\u578b"))
-  expect_equal(nrow(l[[2]]), 3L)
   expect_equal(colnames(l[[2]]), c("\u5c5e\u6027\u540d", "\u8aac\u660e", "\u5c5e\u6027\u306e\u578b"))
 })
 
@@ -26,4 +25,29 @@ test_that("parse_table() works for HTML with table with empty rows", {
   d <- parse_table(rvest::html_nodes(xml2::read_html("with-empty-rows.html"), "tr"))
 
   expect_equal(nrow(d), 2L)
+})
+
+test_that("parse_table() works for HTML with table without rows", {
+  l <- parse_test_table("table-without-rows.html")
+
+  expect_equivalent(purrr::map_int(l, nrow), c(2L, 1L))
+  expect_equal(colnames(l[[1]]), c("\u5c5e\u6027\u540d", "\u8aac\u660e", "\u5c5e\u6027\u306e\u578b"))
+  expect_equal(colnames(l[[2]]), c("\u5c5e\u6027\u540d", "\u8aac\u660e", "\u5c5e\u6027\u306e\u578b"))
+})
+
+test_that("parse_table() works for HTML with table without rows and empty leftmost", {
+  l <- parse_test_table("table-without-rows-and-empty-leftmost.html")
+
+  expect_equivalent(purrr::map_int(l, nrow), c(2L, 1L))
+  expect_equal(colnames(l[[1]]), c("\u5c5e\u6027\u540d", "\u8aac\u660e", "\u5c5e\u6027\u306e\u578b"))
+  expect_equal(colnames(l[[2]]), c("\u5c5e\u6027\u540d", "\u8aac\u660e", "\u5c5e\u6027\u306e\u578b"))
+})
+
+
+test_that("parse_table() works for HTML with table without rows and another leftmost", {
+  l <- parse_test_table("table-without-rows-and-another-leftmost.html")
+
+  expect_equivalent(purrr::map_int(l, nrow), c(2L, 1L))
+  expect_equal(colnames(l[[1]]), c("\u5c5e\u6027\u540d", "\u8aac\u660e", "\u5c5e\u6027\u306e\u578b"))
+  expect_equal(colnames(l[[2]]), c("\u5c5e\u6027\u540d", "\u8aac\u660e", "\u5c5e\u6027\u306e\u578b"))
 })
